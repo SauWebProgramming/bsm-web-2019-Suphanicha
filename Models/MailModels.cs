b779abc6648c5760fd826c2a1fbab9d4ee9bf105
Suphanicha <58908253+Suphanicha@users.noreply.github.com>
2019-12-23 00:32:23 +0300

    Database

---
+++ Models/MailModels.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+
+     public class MailModels
+    {
+        public string Fullname { get; set; }
+        public string Subject { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public string Message { get; set; }
+
+    }
+}
+
---
+++ Models/gmail.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Web;
+
+namespace WebApplication1.Models
+{
+    public class gmail
+    {
+        public static void SendMail(string body)
+        {
+            var fromAddress = new MailAddress("bcscanerweb@gmail.com", "Leaf Solutions Geribildirim");
+            var toAddress = new MailAddress("bcscanerweb@gmail.com");
+            const string subject = "Leaf Solutions Geribildirim";
+            using (var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, "BC1212312121")
+                //trololol kısmı e-posta adresinin şifresi
+            })
+            {
+                using (var message = new MailMessage(fromAddress, toAddress) { Subject = subject, Body = body })
+                {
+                    smtp.Send(message);
+                }
+            }
+        }
+    }
+}
